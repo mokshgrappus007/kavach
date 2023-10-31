ddaf4038fe708e6a8b8ccc556be3c245aa9df1d1
mox <moksh@grappus.com>
2023-10-31 15:39:40 +0530

    Added colors

--- app/src/main/java/com/grappus/kavach/Kavach.kt
+++ app/src/main/java/com/grappus/kavach/Kavach.kt
@@ -4,5 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class Kavach: Application() {
-}
+class Kavach: Application()
--- app/src/main/java/com/grappus/kavach/ui/theme/Color.kt
+++ app/src/main/java/com/grappus/kavach/ui/theme/Color.kt
@@ -9,3 +9,89 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val primaryColor = Color(0xff131415)
+val colorTransparent = Color(0x00000000)
+
+// Snackbar Color
+val snackBarColor = Color(0xB36b6e7e)
+val snackBarRed = Color(0xB3ff4f4f)
+val snackBarGreen = Color(0xB351ff56)
+
+val neon = Color(0xFFECFD33)
+
+// White Shades
+val white = Color(0xFFFFFFFF)
+val whiteOpacity10 = Color(0x1AFFFFFF)
+val whiteOpacity20 = Color(0x33FFFFFF)
+val whiteOpacity30 = Color(0x4DFFFFFF)
+val whiteOpacity40 = Color(0x66FFFFFF)
+val whiteOpacity50 = Color(0x80FFFFFF)
+val whiteOpacity60 = Color(0x99FFFFFF)
+val whiteOpacity70 = Color(0xB3FFFFFF)
+val whiteOpacity80 = Color(0xCCFFFFFF)
+
+val white2 = Color(0xfff0f0f0)
+
+val appBlue = Color(0xFF4C68EF)
+val onboardingIndicator = Color(0xFF4C68EF)
+val onboardingIndicator50 = Color(0x804C68EF)
+val appRed = Color(0xfff95959)
+val vodka = Color(0xffBCC4FF)
+
+// Black Shades
+val black = Color(0xFF000000)
+val blackOpacity5 = Color(0x0D000000)
+val blackOpacity10 = Color(0x1A371C1C)
+val blackOpacity15 = Color(0x26000000)
+val blackOpacity20 = Color(0x33000000)
+val blackOpacity30 = Color(0x4D000000)
+val blackOpacity40 = Color(0x66000000)
+val blackOpacity50 = Color(0x80000000)
+val blackOpacity60 = Color(0x99000000)
+val blackOpacity70 = Color(0xB3000000)
+val blackOpacity80 = Color(0xCC000000)
+val raisinBlack17 = Color(0xFF232320)
+
+val black2 = Color(0xff343434)
+val black3 = Color(0xFF101010)
+
+val backgroundBlack10 = Color(0x1A131415)
+val backgroundBlack40 = Color(0x66131415)
+
+// Grey Shades
+val grey = Color(0xFF232220)
+val surfaceTopRegion = Color(0xFF1F1E1B)
+val raisinBlack = Color(0xFF232220)
+val alloyOrange = Color(0xFFBC6201)
+
+val eerieBlack = Color(0xFF1F1E1B)
+
+val grey1 = Color(0xFF939393)
+val gunMetal = Color(0xFF4C4E59)
+val battleshipGrey = Color(0xFF6B6E7E)
+val shimmerGrey = Color(0xB3262629)
+val blueGrey = Color(0xFF8dadba)
+
+// Gradient Colors
+val gradientStart = Color(0xFF131415)
+val gradientCenter = Color(0xB3151617)
+val gradientEnd = Color(0x4D1B1C1E)
+val vampireBlack = Color(0xFF0B0A07)
+val lightGray = Color(0xffD1D3D9)
+val pinkLace = Color(0xffFFE4FB)
+val oriolesOrange = Color(0xffFF5A10)
+val lavenderIndigo = Color(0xffA66CFE)
+val macaroniCheese = Color(0xffFFB985)
+val mindaro = Color(0xffE9FC88)
+val aeroBlue = Color(0xffBCF4DE)
+val seaGreen = Color(0xff2A8D5C)
+
+// yellow shades
+val cornSilk = Color(0xFFFFFADE)
+
+// pink shade
+val heliotrope = Color(0xFFDE4FF3)
+val brilliantLavender = Color(0xFFFFBBF4)
+val citrine = Color(0xFFD8D701)
+val lightGoldenrodYellow = Color(0xFFF8FFD4)
