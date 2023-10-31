6ab42edef8908838cae2f444508eaf538602c952
mox <moksh@grappus.com>
2023-10-31 16:06:55 +0530

    Added Fonts

--- app/src/main/java/com/grappus/kavach/ui/theme/Type.kt
+++ app/src/main/java/com/grappus/kavach/ui/theme/Type.kt
@@ -2,15 +2,48 @@ package com.grappus.kavach.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.grappus.kavach.R
 
 // Set of Material typography styles to start with
+
+val InterFont = FontFamily(
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_regular),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+)
+
+val BruleFont = FontFamily(
+    Font(R.font.brule_medium, FontWeight.Medium),
+    Font(R.font.brule_semibold, FontWeight.SemiBold),
+)
+
+val LalezarFont = FontFamily(
+    Font(R.font.lalezar_regular)
+)
+
+val LuckiestGuyFont = FontFamily(
+    Font(R.font.luckiestguy_regular)
+)
+
+val NeueHaasUnicaFont = FontFamily(
+    Font(R.font.neuehaasunicapro_regular),
+    Font(R.font.neuehaasunicapro_bold, FontWeight.Bold),
+    Font(R.font.neuehaasunicapro_light, FontWeight.Light),
+)
+
+val QuickSandFont = FontFamily(
+    Font(R.font.quicksand_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = BruleFont,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
