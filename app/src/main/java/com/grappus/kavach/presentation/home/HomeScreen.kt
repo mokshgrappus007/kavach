f04ddad77b7deed22dd10d36d9015a2631f34cd0
Vishal <vishal.rao103@gmail.com>
2023-10-31 21:49:34 +0530

    make surface default background color

---
+++ app/src/main/java/com/grappus/kavach/presentation/home/HomeScreen.kt
@@ -0,0 +1,28 @@
+package com.grappus.kavach.presentation.home
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.grappus.kavach.ui.theme.KavachColor
+import com.grappus.kavach.ui.theme.KavachTheme
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    KavachTheme(KavachTheme.darkColorScheme, true) {
+        HomeScreenBody()
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun HomeScreenBody() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "Hello Everyone",
+        )
+    }
+}
