fd2da083d7a9fe9bd0f3079bd5de19566aca0693
Vishal <vishal.rao103@gmail.com>
2023-10-31 16:49:38 +0530

    disabled dark and dynamic themes

--- app/src/main/java/com/grappus/kavach/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/grappus/kavach/presentation/home/HomeScreen.kt
@@ -1,20 +1,27 @@
 package com.grappus.kavach.presentation.home
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.grappus.kavach.ui.theme.KavachTheme
 
 @Composable
-fun HomeScreen(
-    navController: NavController
-){
-    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        Text(
-            text = "Hello EveryOne!",
-        )
+fun HomeScreen(navController: NavController) {
+    HomeScreenBody()
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun HomeScreenBody() {
+    KavachTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "Hello Everyone",
+            )
+        }
     }
 }
