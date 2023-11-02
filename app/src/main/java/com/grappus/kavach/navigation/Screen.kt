91ce085a3217b323b25a1d35c7e8ebc2df75f0ec
mox <moksh@grappus.com>
2023-11-02 14:53:20 +0530

    NavGraph fix

--- app/src/main/java/com/grappus/kavach/navigation/Screen.kt
+++ app/src/main/java/com/grappus/kavach/navigation/Screen.kt
@@ -3,4 +3,5 @@ package com.grappus.kavach.navigation
 sealed class Screen(var route: String) {
     object PhoneVerificationScreen : Screen(route = "phone_verification_screen")
     object DashboardScreen : Screen(route = "dashboard_screen")
+    object ReadScreen : Screen(route = "read_screen")
 }
