c745e9929207b301a46205b43575de80e0f0013f
mox <moksh@grappus.com>
2023-11-03 11:57:55 +0530

    refactor

--- app/src/main/java/com/grappus/kavach/navigation/Screen.kt
+++ app/src/main/java/com/grappus/kavach/navigation/Screen.kt
@@ -3,6 +3,6 @@ package com.grappus.kavach.navigation
 sealed class Screen(var route: String) {
     object DashboardScreen : Screen(route = "dashboard_screen")
     object ReadScreen : Screen(route = "read_screen")
-    object LoginScreen : Screen(route = "login")
+    object LoginScreen : Screen(route = "login_screen")
     object DashboardNestedScreen : Screen("dashboard_nested")
 }
