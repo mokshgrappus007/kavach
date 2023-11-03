754f591fbb0eec3be73c5639d678ebbc06815bcd
mox <moksh@grappus.com>
2023-11-03 20:35:08 +0530

    rebase

--- app/src/main/java/com/grappus/kavach/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/grappus/kavach/presentation/dashboard/DashboardScreen.kt
@@ -1,56 +1,26 @@
 package com.grappus.kavach.presentation.dashboard
 
 import android.net.Uri
-import android.util.Log
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.updateTransition
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.KeyboardArrowLeft
-import androidx.compose.material3.Card
+import androidx.compose.material3.*
 import androidx.compose.material3.CardDefaults.cardElevation
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ScrollableTabRow
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -113,7 +83,7 @@ private fun TabBody(selectedIndex: State<Int>, viewModel: DashboardViewModel, na
     Box {
         when {
             uiState.data?.data?.content?.isNotEmpty() == true -> {
-                CardItemList(uiState.data!!.data.content , navController = navController)
+                CardItemList(uiState.data!!.data.content, selectedIndex = selectedIndex, navController = navController)
             }
 
             else -> Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
@@ -160,7 +130,7 @@ private fun CardItemList(contentList: List<ContentListData>, selectedIndex: Stat
                 description = content.description,
                 isPortrait = content.isPortrait,
                 readTime = content.readTime,
-                thumbnail =  Uri.encode(content.thumbnail),
+                thumbnail = Uri.encode(content.thumbnail),
                 title = content.title,
                 updatedAt = content.updatedAt
             )
@@ -170,21 +140,20 @@ private fun CardItemList(contentList: List<ContentListData>, selectedIndex: Stat
                 contentType = content.category,
                 imageUrl = content.thumbnail,
                 modifier = Modifier.clickable {
-                    navController.navigate(route =  Screen.DetailScreen.route.replace("{content}", contentStr))
-                }
+                    navController.navigate(route = Screen.DetailScreen.route.replace("{content}", contentStr))
+                },
                 date = content.createdAt
             )
         }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun CardItem(
+    modifier: Modifier = Modifier,
     heading: String,
     contentType: String,
     imageUrl: String? = null,
-    modifier: Modifier = Modifier,
     date: String
 ) {
     Card(
